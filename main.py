1198c88bea7dcde2b92a156c53cf8b4b40d5a0ae
AsumVictor <iamasum369@gmail.com>
2024-02-21 01:25:08 +0000

    Added function layout

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from userAccount import UserAccount
-message  = '''
+
+def main():
+ message  = '''
            HELLO HAPPY CUSTOMER!
     ----------Welcome M-MoMo Application-------
     
     You have create account before you can use.
            '''
            
-print(message)
+ print(message)
+ print(' ')
+
+ response = input('Press 1 to continue creating an account else press any key to exist')
+
+ if not response == '1':
+    return 0
+
+ quiting = False
+ while not quiting:
+     print(' Yes')
+    
+main()
+
