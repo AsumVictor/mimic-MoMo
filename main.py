cd1fae07650fa577e3acc4fcbeff5f688dfd5758
AsumVictor <iamasum369@gmail.com>
2024-02-21 01:16:37 +0000

    Initialed main

---
+++ main.py
@@ -0,0 +1,9 @@
+from userAccount import UserAccount
+message  = '''
+           HELLO HAPPY CUSTOMER!
+    ----------Welcome M-MoMo Application-------
+    
+    You have create account before you can use.
+           '''
+           
+print(message)
