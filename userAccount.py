fc4913b8226b8ea3e8b0d74d74e34ded68fd38d5
AsumVictor <iamasum369@gmail.com>
2024-02-21 01:08:14 +0000

    Added get detais method

--- userAccount.py
+++ userAccount.py
@@ -5,3 +5,22 @@ class UserAccount:
         self.Dob  = Dob
         self.Pin  = Pin
         self.balance  = balance
+    
+    def get_detail(self):
+        return f''' 
+                 Your name: {self.name},
+                 Mobile Number: {self.mobile_number}
+                 Date Of Birth: {self.Dob}
+                 PIN: {self.Pin}
+                 Current Balance: {self.balance}
+                '''
+                
+    def changePin(self):
+        oldpin = input('Enter your old pin')
+        if not oldpin == self.Pin:
+            return 'Invalid old PIN'
+        
+        new_pin = input('Enter your old pin')
+        confirm_new_pin = input('Enter your old pin')
+        
+            
