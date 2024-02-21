0bc04256de2e9b0ffa4a71401ed70d1e694033e7
AsumVictor <iamasum369@gmail.com>
2024-02-21 00:58:57 +0000

    Initialized user class

---
+++ userAccount.py
@@ -0,0 +1,7 @@
+class UserAccount: 
+    def __init__(self, name, mobile_number, Dob, Pin, balance ):
+        self.name  = name
+        self.mobile_number  = mobile_number
+        self.Dob  = Dob
+        self.Pin  = Pin
+        self.balance  = balance
